45161318a51f4575463db2ece2fc47a13bebb78a
pawelwuuu <wojcikp376@gmail.com>
2024-06-08 22:21:24 +0200

    changed paths of api router to contain full api path

--- src/routes/apiRouter.js
+++ src/routes/apiRouter.js
@@ -4,8 +4,8 @@ const path = require('node:path');
 const apiController = require(path.join(__dirname, '..', 'controllers', 'apiController'))
 
 
-apiRouter.get('/documents', apiController.documentsApi);
+apiRouter.get('/api/documents', apiController.documentsApi);
 
-apiRouter.get('/documents/:id', apiController.documentByIdApi)
+apiRouter.get('/api/documents/:id', apiController.documentByIdApi)
 
 module.exports = apiRouter
